e270248571fb38fa828bf62f6924e25d5bc74481
Jonathan <jonathanmolinadejesus312@gmail.com>
2025-08-27 07:48:50 -0600

    Fix: Servicios y fuentes PrimeIcons corregidos

--- Front/gui/src/app/core/services/task.service.ts
+++ Front/gui/src/app/core/services/task.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
+import { environment } from '../../../environments/environment';
 
 export interface Task {
   id?: number;
--- Front/gui/src/app/core/services/user.service.ts
+++ Front/gui/src/app/core/services/user.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
+import { environment } from '../../../environments/environment';
 
 export interface User {
   id: number;
